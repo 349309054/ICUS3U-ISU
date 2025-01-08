d8b4b3d87923826dd2f4191d3d59a9e9ba7b08d6
349309054 <349309054@gapps.yrdsb.ca>
2025-01-08 18:38:03 +0000

    Adjusted some code

--- Soccer Tourdement/src/Main.java
+++ Soccer Tourdement/src/Main.java
@@ -1,40 +1,49 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       
-
-         // Store the team names
+       Scanner sc = new Scanner(System.in);
+        // Store the team names
         String[] team_list = new String[8];
-        for (int i = 0; i < 8; i ++){
-            System.out.println("please enter the name of team " + (i+1));
+        for (int i = 0; i < 8; i++) {
+            System.out.println("Please enter the name of team " + (i + 1));
             String name = sc.nextLine();
-            if (i-1 >= 0){
-                //Ensure uniqueness
-                if(!name.equals(team_list[i-1])){
-                    team_list[i] = name;
-                }
-                else{
-                    System.out.println("please ensure the team names are unique");
+            //Looking at Uniqueness
+            boolean isUnique = true;
+            for (int j = 0; j < i; j++) {
+                if (name.equals(team_list[j])) {
+                    isUnique = false;
                     break;
                 }
-            }
-            else{
-            team_list[i] = name;
+            } if (isUnique) {
+                team_list[i] = name;
+            } else {
+                System.out.println("Please ensure the team names are unique");
+                i--; 
             }
         }
 
         //Confirmation
         for (String i: team_list){
             System.out.print(i);
-            System.out.print(" ");
+            System.out.print(", ");
         }
-        System.out.println("enter \" confirm\" to continue");
+        System.out.println("enter \"confirm\" to continue");
         if (sc.nextLine().equals("confirm")){
             System.out.println("Continuing");
         }
         else{
             System.out.println("Please exit and re-enter the name");
         }
+
+        //Assign teams
+        String[] group1 = new String[4];
+        String[] group2 = new String[4];
+        int counter = 0;
+        while(counter < 4){
+            int n = (int)(Math.random()*(8));
+            // seprate and check if team is in a geoup or not repated
+        }
+
 
         
     }
