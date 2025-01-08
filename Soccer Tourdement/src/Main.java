5fadce8b432be2a4ddb6770dbb1913384cf39250
349309054 <349309054@gapps.yrdsb.ca>
2025-01-08 17:46:08 +0000

    Assigned team names and confirmation

--- Soccer Tourdement/src/Main.java
+++ Soccer Tourdement/src/Main.java
@@ -1,9 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+       
+
+         // Store the team names
+        String[] team_list = new String[8];
+        for (int i = 0; i < 8; i ++){
+            System.out.println("please enter the name of team " + (i+1));
+            String name = sc.nextLine();
+            if (i-1 >= 0){
+                //Ensure uniqueness
+                if(!name.equals(team_list[i-1])){
+                    team_list[i] = name;
+                }
+                else{
+                    System.out.println("please ensure the team names are unique");
+                    break;
+                }
+            }
+            else{
+            team_list[i] = name;
+            }
+        }
+
+        //Confirmation
+        for (String i: team_list){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+        System.out.println("enter \" confirm\" to continue");
+        if (sc.nextLine().equals("confirm")){
+            System.out.println("Continuing");
+        }
+        else{
+            System.out.println("Please exit and re-enter the name");
+        }
+
         
     }
 }
